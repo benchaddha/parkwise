3a95375654c3141c33d57a1501c58f426cbe3241
liam-powers <95184359+liam-powers@users.noreply.github.com>
2024-04-07 10:27:28 -0500

    mostly working navbar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,80 +1,50 @@
-import React from "react";
-import { Image } from "./image";
+const Navigation = () => {
+  const linkCSS = "flex flex-col items-center text-[#333] hover:text-blue-700 hover:text-white";
 
-export const Navigation = (props) => {
   return (
-    <nav id="menu" className="text-black navbar navbar-default navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-          </button>{" "}
-        </div>
-
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <a className="navbar-brand page-scroll" href="#page-top">
-            <img
-              src="./logos/parkwise-colored-transparent.png"
-              alt="logo"
-              style={{ width: "100px", height: "100px" }}
-            />
-          </a>
-          <ul className="nav navbar-nav navbar-right">
-            <Image
-              src="./parkwise_colored.jpeg"
-              className="img-responsive"
-              alt="logo"
-            />
-            <li>
-              <a href="#features" className="page-scroll">
-                Features
-              </a>
-            </li>
-            <li>
-              <a href="#about" className="page-scroll">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="page-scroll">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#portfolio" className="page-scroll">
-                Demo
-              </a>
-            </li>
-            {/* <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonials
-              </a>
-            </li> */}
-            <li>
-              <a href="#team" className="page-scroll">
-                Team
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="page-scroll">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div className="w-full h-[60px] bg-white sticky top-0 text-4xl z-10">
+      <div className="h-full flex justify-center items-center">
+        <img className="flex justify-start items-center absolute top-4 left-4" src="/Logo.webp" alt="Logo" width={50} height={50}/>
+        <ul className="flex flex-row gap-20">
+          <li className="flex items-center">
+            <a href="" className={linkCSS}>
+              Home
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a href="#features" className={linkCSS}>
+              Features
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a href="#about" className={linkCSS}>
+              About
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a href="#services" className={linkCSS}>
+              Services
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a href="#portfolio" className={linkCSS}>
+              Demo
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a href="#team" className={linkCSS}>
+              Team
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a href="#contact" className={linkCSS}>
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </div>
   );
-};
+}
+
+export default Navigation;
